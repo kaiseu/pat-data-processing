7b798e5b216f1e293a87cc6085487e10379159da
xuk1 <kai.a.xu@intel.com>
2017-12-12 14:47:49 +0800

    resolve different platform memory raw data not consistent issue

--- component/mem.py
+++ component/mem.py
@@ -21,9 +21,8 @@ class Mem(CommonBase):
     """
     Node memory attribute, phasing memory data from original PAT file
     """
-    used_col = [0, 1, 13, 14, 16, 17]  # names = ['TimeStamp', 'kbmemfree', 'kbmemused', 'kbbuffers', 'kbcached']
-    names = ['HostName', 'TimeStamp', 'kbmemfree', 'kbmemused', 'kbbuffers', 'kbcached']
-    converter = {col: np.int64 for col in names[2:]}
+    used_col = ['HostName', 'TimeStamp', 'kbmemfree', 'kbmemused', 'kbbuffers', 'kbcached']
+    converter = {col: np.int64 for col in used_col[2:]}
 
     def __init__(self, file_path):
         self.file_path = file_path
@@ -37,22 +36,22 @@ class Mem(CommonBase):
         :return: dict that contains avg value of all the timestamp pair and all raw data
         """
         df = pd.read_csv(self.file_path, delim_whitespace=True,
-                         usecols=self.used_col, names=self.names, header=0)
+                         usecols=self.used_col, header=0)
         df = df.loc[0::2]  # read every two rows
         pd.to_datetime(df['TimeStamp'], unit='s')
         df = df.set_index('TimeStamp').astype(self.converter)
         avg = []
         if start[0] == end[0] == 0:  # calc all the data
-            avg.append(df.iloc[:, 1:len(self.used_col)].mean(axis=0).astype('float32'))
+            avg.append(df.iloc[:, 2:len(self.used_col)].mean(axis=0).astype('float32'))
             if len(start) == 1:
                 return avg, df
             else:
                 for i in range(1, len(start)):  # calc the data within the pair of time period
-                    avg.append(df.loc[str(start[i]): str(end[i])].iloc[:, 1:len(self.used_col)].mean(axis=0))
+                    avg.append(df.loc[str(start[i]): str(end[i])].iloc[:, 2:len(self.used_col)].mean(axis=0))
                 return avg, df
 
         for i in range(len(start)):  # calc the data within the pair of time period
-            avg.append(df.loc[str(start[i]): str(end[i]), self.names[2:]].mean(axis=0).astype('float32'))
+            avg.append(df.loc[str(start[i]): str(end[i]), self.used_col[2:]].mean(axis=0).astype('float32'))
         return avg, df
 
     def used_col_num(self):
